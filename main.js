7091f46381a905444dc56c710645ac9758cdd377
grayTurtle01 <the.correo@protonmail.com>
2022-05-24 02:46:20 -0500

    flip coin frontEnd

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector("button").onclick = function(){
+    url = 'http://http-server-node.herokuapp.com/api'
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( data => {
+        console.log(data)
+        document.querySelector("#coin-value").innerText = data.flag
+    })
+
+}
