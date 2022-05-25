ece0033ffce714512f7d1e23e6c3331ad282846b
grayTurtle01 <the.correo@protonmail.com>
2022-05-25 16:15:10 -0500

    coin images

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ document.querySelector("button").onclick = function(){
         console.log(data)
         document.querySelector("#random").innerText = data.random
         document.querySelector("#coin-value").innerText = data.flag
+
+
+        img = document.querySelector("img")
+
+        if( data.flag )
+            img.src = "images/head.jpeg"
+        else
+            img.src = "images/tail.jpeg"
+            
+        
     })
 
 }
