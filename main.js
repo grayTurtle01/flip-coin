608cd4d3b542dfade3f99addf7eb5d4426970ae7
grayTurtle01 <the.correo@protonmail.com>
2022-05-29 03:41:48 -0500

    show counter with format

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ document.querySelector("button").onclick = function(){
 
 
         //update counter values
-        document.querySelector("#heads").innerText = data.counter.heads    
-        document.querySelector("#tails").innerText = data.counter.tails    
+        document.querySelector("#heads").innerText = 'Heads: ' + data.counter.heads    
+        document.querySelector("#tails").innerText = 'Tails: ' + data.counter.tails    
         
     })
     .catch(err => console.log(err))
